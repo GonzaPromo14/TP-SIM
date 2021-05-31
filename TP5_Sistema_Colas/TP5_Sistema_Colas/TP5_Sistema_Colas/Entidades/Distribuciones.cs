2edde112546f57ca7083d17ad7428a17e14bbd08
Gonzalo Martínez <martinez.gabriel.gonzalo@gmail.com>
2021-05-31 12:26:37 -0300

    Implementación de distribuciones

---
+++ TP5_Sistema_Colas/TP5_Sistema_Colas/TP5_Sistema_Colas/Entidades/Distribuciones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP5_Sistema_Colas.Entidades
+{
+    public static class Distribuciones
+    {
+
+        public static double Exponencial(double rnd, double media)
+        {
+            return ((-1) * media) * Math.Log(1 - rnd);
+        }
+
+        public static double NormalDist(double rnd1, double rnd2, double media, double desvEstandar)
+        {
+            double raiz = Math.Sqrt((-2) * Math.Log(rnd1));
+            double cos = Math.Cos(2 * Math.PI * rnd2);
+            return (raiz * cos * desvEstandar) + media;
+        }
+
+    }
+}
